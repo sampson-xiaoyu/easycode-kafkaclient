64f5f3e054a6b132bba2821f7b21251ae4c392ee
xiaoyu <xiaoyu@192.168.116.1>
2016-11-18 01:00:16 +0800

    更新

--- src/main/java/com/easycode/message/kafka/message/manager/impl/MessageManagerImpl.java
+++ src/main/java/com/easycode/message/kafka/message/manager/impl/MessageManagerImpl.java
@@ -1,20 +1,27 @@
 package com.easycode.message.kafka.message.manager.impl;
 
-import javax.annotation.Resource;
-
+import com.easycode.commons.json.JSONUtils;
 import com.easycode.message.kafka.message.Message;
 import com.easycode.message.kafka.message.manager.MessageManager;
 import com.easycode.message.kafka.template.ProducerTemplate;
 
 public class MessageManagerImpl implements MessageManager{
 	
-	@Resource(name = "producerTemplate")
     private ProducerTemplate producerTemplate;
 	
 	@Override
 	public void sendUserMessage(String topic,Message message) {
 		// TODO Auto-generated method stub
-		producerTemplate.send(topic, System.currentTimeMillis()+"", message.toString());
+		producerTemplate.send(topic, System.currentTimeMillis()+"", JSONUtils.toJSON(message));
 	}
 
+	public ProducerTemplate getProducerTemplate() {
+		return producerTemplate;
+	}
+
+	public void setProducerTemplate(ProducerTemplate producerTemplate) {
+		this.producerTemplate = producerTemplate;
+	}
+	
+	
 }
